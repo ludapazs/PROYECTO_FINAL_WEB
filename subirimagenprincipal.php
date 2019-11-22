48436d7f26f2012e8cb4f0e1396ecec6ec7113d8
piscoyron <js360990@gmail.com>
2019-11-21 21:17:23 -0500

    asasas

--- subirimagenprincipal.php
+++ subirimagenprincipal.php
@@ -52,7 +52,34 @@ else
    if($nombre_imgp == !NULL) echo "La imagen es demasiado grande ";
 }
 
-
+if (($nombre_imgd1 == !NULL) && ($tamanod1 <= 200000))
+{
+   //indicamos los formatos que permitimos subir a nuestro servidor
+   if (($tipod1 == "image/gif")
+   || ($tipod1 == "image/jpeg")
+   || ($tipod1 == "image/jpg")
+   || ($tipod1 == "image/png"))
+   {
+      // Ruta donde se guardarán las imágenes que subamos
+      $directoriod1 = $_SERVER['DOCUMENT_ROOT'].'/proyecto';
+      // Muevo la imagen desde el directorio temporal a nuestra ruta indicada anteriormente
+      move_uploaded_file($_FILES['imagend2']['tmp_name'],$directoriod1.$nombre_imgd1);
+      $sql = "UPDATE placa_madre SET img2 = '$nombre_imgd1' where id = (select MAX(id) from placa_madre)";
+      $resp=1;
+        $cnx->query($sql) or $resp=0;
+        echo "<p>Subida satisfactoria $resp</p>";
+    }
+    else
+    {
+       //si no cumple con el formato
+       echo "No se puede subir una imagen con ese formato ";
+    }
+}
+else
+{
+   //si existe la variable pero se pasa del tamaño permitido
+   if($nombre_imgd2 == !NULL) echo "La imagen es demasiado grande ";
+}
 
 if (($nombre_imgd2 == !NULL) && ($tamanod2 <= 200000))
 {
@@ -130,7 +157,7 @@ if (($nombre_imgd4 == !NULL) && ($tamanod4 <= 200000))
       $directoriod4 = $_SERVER['DOCUMENT_ROOT'].'/proyecto';
       // Muevo la imagen desde el directorio temporal a nuestra ruta indicada anteriormente
       move_uploaded_file($_FILES['imagend4']['tmp_name'],$directoriod4.$nombre_imgd4);
-      $sql = "UPDATE placa_madre SET img4 = '$nombre_imgd4' where id = (select MAX(id) from placa_madre)";
+      $sql = "UPDATE placa_madre SET img5 = '$nombre_imgd4' where id = (select MAX(id) from placa_madre)";
       $resp=1;
         $cnx->query($sql) or $resp=0;
         echo "<p>Subida satisfactoria $resp</p>";
